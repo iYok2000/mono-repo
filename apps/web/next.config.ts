842b04afa8ae524c2e7bedfd3af232828b132b91
Yokky <noppachai@Noppachais-MacBook-Air.local>
2025-12-19 09:36:59 +0700

    update dockerfile and docker-compose

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'standalone', // For Docker optimization
 };
 
 export default nextConfig;
